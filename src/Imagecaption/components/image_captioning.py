0933c50c463942ec9aa4bb1e7cf2b6d59eb4971c
Himanshu Singh <Singhhimanshu33456@gmail.com>
2026-06-23 00:39:04 +0530

    Switch Florence-2 model to base-ft and support dynamic float16/float32 precision for GPU/CPU compatibility

--- src/Imagecaption/components/image_captioning.py
+++ src/Imagecaption/components/image_captioning.py
@@ -10,8 +10,14 @@ class ImageCaptioning:
     def __init__(self, config):
         self.config = config
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.processor = AutoProcessor.from_pretrained(config.florence2_model_name, trust_remote_code=True, revision= "4a12a2b")
-        self.model = AutoModelForCausalLM.from_pretrained(config.florence2_model_name, trust_remote_code=True, revision="4a12a2b" ).to(self.device)
+        self.processor = AutoProcessor.from_pretrained(config.florence2_model_name, trust_remote_code=True, revision="main")
+        torch_dtype = torch.float16 if self.device.type != "cpu" else torch.float32
+        self.model = AutoModelForCausalLM.from_pretrained(
+            config.florence2_model_name, 
+            trust_remote_code=True, 
+            torch_dtype=torch_dtype, 
+            revision="main"
+        ).to(self.device).eval()
         self.config.captions_dir.mkdir(parents=True, exist_ok=True)
 
     def caption_image(self, image_path: Path) -> str:
